63a644e25f3487f3f5c39dde3a821604d0112418
Eloi Puertas <eloipuertas@gmail.com>
2022-11-21 12:33:09 +0000

    DevContainer added

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return ("Hello world. I finished Enriques homework!!!! Remember to change this")
+    return "Hello world. I finished Enriques homework!!!! Remember to change this"
 
-
-
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
